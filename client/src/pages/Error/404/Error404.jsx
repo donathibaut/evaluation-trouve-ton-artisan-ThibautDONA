522789cc2f16f74af9f28854240bbe5ed49431a5
ThibautDona <donathibaut02@gmail.com>
2026-03-25 22:13:19 +0100

    error404 page ok

--- client/src/pages/Error/404/Error404.jsx
+++ client/src/pages/Error/404/Error404.jsx
@@ -1,6 +1,8 @@
 import { Helmet } from "react-helmet-async";
 import error404 from "../../../assets/img/error404.png";
 
+import "./Error404.scss";
+
 export default function Error404() {
   return (
     <>
@@ -8,8 +10,8 @@ export default function Error404() {
         <title>Not Found</title>
         <meta name="description" content="Erreur 404 : Not Found" />
       </Helmet>
-      <main>
-        <section>
+      <main className="errorMain">
+        <section className="error">
           <img src={error404} alt="erreur 404" />
           <h1>Page non trouvée</h1>
         </section>
